4ad324045e81378e4871a45bca79918f65569eec
Jairo <jairosantos08@gmail.com>
2021-06-02 05:22:44 +0000

    005 actually stretching the bitmap

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use bindings::{
         RegisterClassExW, TranslateMessage, SetWindowLongW, GetWindowLongW, PeekMessageW, CW_USEDEFAULT, HWND,
         LPARAM, MSG, WINDOW_EX_STYLE, WM_ACTIVATEAPP, WM_CLOSE, WM_PAINT, WM_SIZE,
         WNDCLASSEXW, WNDCLASS_STYLES, WPARAM, WS_OVERLAPPEDWINDOW, WS_VISIBLE, GWLP_USERDATA, WM_CREATE,
-        CREATESTRUCTW, WM_DESTROY, PM_REMOVE
+        CREATESTRUCTW, WM_DESTROY, PM_REMOVE, CS_HREDRAW, CS_VREDRAW
     },
     Windows::Win32::UI::DisplayDevices::RECT,
 };
@@ -52,6 +52,8 @@ struct Win32Game {
     bitmap_info: BITMAPINFO,
     bitmap_mem: std::vec::Vec::<u32>,
     window: HWND,
+    window_width: u32,
+    window_height: u32
 }
 
 fn win32_get_game(window: HWND) -> &'static mut Win32Game {
@@ -68,9 +70,9 @@ fn win32_render(game: &Win32Game) {
         let r = StretchDIBits(
             hdc,
             0,
-            game.bitmap_info.bmiHeader.biHeight,
-            game.bitmap_info.bmiHeader.biWidth,
-            -game.bitmap_info.bmiHeader.biHeight,
+            game.window_height as i32,
+            game.window_width as i32,
+            -(game.window_height as i32),
             0,
             0,
             game.bitmap_info.bmiHeader.biWidth,
@@ -100,6 +102,30 @@ fn win32_render_weird_gradient(game: &mut Win32Game, xoffset: i32, yoffset: i32)
     }
 }
 
+fn win32_resize_bitmap_buffer(game: &mut Win32Game, width: u32, height: u32) {
+
+    game.bitmap_info = BITMAPINFO {
+        bmiHeader: BITMAPINFOHEADER {
+            biSize: std::mem::size_of::<BITMAPINFOHEADER>() as u32,
+            biWidth: width as i32,
+            biHeight: height as i32,
+            biPlanes: 1,
+            biBitCount: 32,
+            biCompression: BI_RGB as u32,
+            biSizeImage: 0,
+            biClrImportant: 0,
+            biClrUsed: 0,
+            biXPelsPerMeter: 0,
+            biYPelsPerMeter: 0,
+        },
+        bmiColors: [RGBQUAD::default()],
+    };
+
+    let bitmap_size_pixels = game.bitmap_info.bmiHeader.biWidth * game.bitmap_info.bmiHeader.biHeight;
+
+    game.bitmap_mem = vec![0; bitmap_size_pixels as usize];
+}
+
 extern "system" fn window_event_handler(
     window: HWND,
     message: u32,
@@ -120,35 +146,14 @@ extern "system" fn window_event_handler(
             LRESULT::default()
         }
         WM_SIZE => {
-            debug!("resizing GDI buffer");
 
             let game = win32_get_game(window);
 
-            let mut rect = RECT::default();
-            unsafe {
-                GetClientRect(window, &mut rect);
-            }
+            let mut rect= RECT::default();
+            unsafe { GetClientRect(window, &mut rect) };
 
-            game.bitmap_info = BITMAPINFO {
-                bmiHeader: BITMAPINFOHEADER {
-                    biSize: std::mem::size_of::<BITMAPINFOHEADER>() as u32,
-                    biWidth: rect.right - rect.left,
-                    biHeight: rect.bottom - rect.top,
-                    biPlanes: 1,
-                    biBitCount: 32,
-                    biCompression: BI_RGB as u32,
-                    biSizeImage: 0,
-                    biClrImportant: 0,
-                    biClrUsed: 0,
-                    biXPelsPerMeter: 0,
-                    biYPelsPerMeter: 0,
-                },
-                bmiColors: [RGBQUAD::default()],
-            };
-
-            let bitmap_size_pixels = game.bitmap_info.bmiHeader.biWidth * game.bitmap_info.bmiHeader.biHeight;
-
-            game.bitmap_mem = vec![0; bitmap_size_pixels as usize];
+            game.window_width = (rect.right - rect.left) as u32;
+            game.window_height = (rect.bottom - rect.top) as u32;
 
             LRESULT::default()
         }
@@ -189,7 +194,7 @@ fn main() -> windows::Result<()> {
 
     let window_template = WNDCLASSEXW {
         cbSize: std::mem::size_of::<WNDCLASSEXW>() as u32,
-        style: WNDCLASS_STYLES::default(),
+        style: CS_HREDRAW | CS_VREDRAW,
         lpfnWndProc: Some(window_event_handler),
         cbClsExtra: 0,
         cbWndExtra: 0,
@@ -211,6 +216,8 @@ fn main() -> windows::Result<()> {
             bitmap_info: BITMAPINFO::default(),
             bitmap_mem: std::vec::Vec::new(),
             window: HWND::default(),
+            window_width: 1280,
+            window_height: 720,
         };
 
         let hwnd = CreateWindowExW(
@@ -220,17 +227,20 @@ fn main() -> windows::Result<()> {
             WS_OVERLAPPEDWINDOW | WS_VISIBLE,
             CW_USEDEFAULT,
             CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
+            game.window_width as i32,
+            game.window_height as i32,
             None,
             None,
             h_instance,
             &mut game as *mut _ as _,
         );
 
-        game.window = hwnd;
 
         debug_assert!(hwnd.0 != 0);
+
+        game.window = hwnd;
+
+        win32_resize_bitmap_buffer(&mut game, 1280, 720);
 
         let mut msg = MSG::default();
         let mut x_offset = 10;
